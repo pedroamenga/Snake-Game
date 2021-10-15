adbcff7e2123ca4b48f3164131d99da6a96e973a
pedroamenga <pedroam92@gmail.com>
2021-10-15 01:42:48 -0300

    primeras etapas

---
+++ game.js
@@ -0,0 +1,29 @@
+import {update as updateSnake, draw as drawSnake,
+SNAKE_SPEED} from './snake.js'
+
+
+let lastRenderTime = 0
+const gameBoard = document.getElementById('gameBoard')
+
+function main(currentTime) {
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    
+    
+    console.log(currentTime)
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+}
